eea4db1c7b6ffcee79415a4a7c91830fb3036303
Ananyobrata Pal <ananyo141@gmail.com>
2023-04-09 00:34:41 +0530

    [FRONTEND] Add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/Login";
 import ProfilePage from "./pages/Profile";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="App">
-      <div className="relative z-50 h-12">
-        <Navbar className="fixed w-full bg-white" />
-      </div>
+      {loggedIn && (
+        <div className="relative z-50 h-12">
+          <Navbar className="fixed w-full bg-white" />
+        </div>
+      )}
       <Router>
         <Routes>
           <Route path="/" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/profile" element={<div>Profile</div>} />
           <Route path="/followers" element={<div>follwers</div>} />
         </Routes>
