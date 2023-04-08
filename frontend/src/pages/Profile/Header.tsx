e5d8d95ee3228c4b56876e8d759bc3071f47fcf9
Ananyobrata Pal <ananyo141@gmail.com>
2023-04-08 12:17:50 +0530

    feat: add followers stat in header and white       gradient like the design

--- frontend/src/pages/Profile/Header.tsx
+++ frontend/src/pages/Profile/Header.tsx
@@ -10,18 +10,23 @@ const Header = (props: Props) => {
   return (
     <div className={props.className}>
       <div className="bg-[url('assets/profile_bg.png')] bg-cover bg-center">
-        <div className="bg-gradient-to-r from-white to-transparent p-6">
-          <div className="relative flex gap-8">
-            <img src={logo} className="h-32 w-32" alt="logo" />
-            <MdModeEdit
-              color="white"
-              className="absolute -bottom-4 left-12 h-9 w-9 cursor-pointer rounded-full bg-[#222831] p-1 text-2xl text-white"
-            />
-            <div>
-              <p className="text-xl">Hello,</p>
-              <p className="text-3xl font-bold">CipherSchools</p>
-              <p className="text-lg italic">*******@cipherschools.com</p>
+        <div className="flex justify-between">
+          <div className="bg-gradient-to-r from-white to-transparent p-6">
+            <div className="relative flex gap-8">
+              <img src={logo} className="h-32 w-32" alt="logo" />
+              <MdModeEdit
+                color="white"
+                className="absolute -bottom-4 left-12 h-9 w-9 cursor-pointer rounded-full bg-[#222831] p-1 text-2xl text-white"
+              />
+              <div>
+                <p className="text-xl">Hello,</p>
+                <p className="text-3xl font-bold">CipherSchools</p>
+                <p className="text-lg italic">*******@cipherschools.com</p>
+              </div>
             </div>
+          </div>
+          <div className="flex items-center bg-gradient-to-r from-transparent to-white px-20 py-6">
+            <h3 className="text-xl font-medium">234 Followers</h3>
           </div>
         </div>
       </div>
