59e972313c96e4d4d97d921022b66382afccfc17
Ananyobrata Pal <ananyo141@gmail.com>
2023-04-13 04:04:20 +0530

    [DEPLOY] connect mongodb in listen callback

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,10 +29,9 @@ app.use(errorHandler);
 const port = process.env.PORT || 8000;
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
-    console.log("Connected to MongoDB");
     app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
+      console.log("Connected to MongoDB");
+      mongoose.connect(process.env.MONGO_URI!);
     });
   } catch (err) {
     console.error(err);
